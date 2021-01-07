46a2783eda85743655ee6bce2a930013c994cd1d
Dzirt2006 <dzirt-2006@mail.ru>
2021-01-07 10:05:04 -0500

    Only spec chars in pool name Test

---
+++ src/test/java/tests/pool/SpecCharsPoolName.java
@@ -0,0 +1,35 @@
+package tests.pool;
+
+import org.openqa.selenium.NoAlertPresentException;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import Pages.PoolsPageActions;
+
+public class SpecCharsPoolName extends PoolsBaseTest {
+	String poolName = "!@#$%^&*()+{}<>./";
+	PoolsPageActions page;
+
+	@Test
+	public void onlySpecCharsPoolName() {
+		String expected = "Please enter valid pool name.";
+		String actual = page.addPool(poolName).getAlert();
+		Assert.assertEquals(actual, expected);
+	}
+
+	@BeforeMethod
+	public void pageIni() {
+		page = new PoolsPageActions(driver);
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		try {
+			page.dismissAlert();
+		} catch (NoAlertPresentException err) {
+			System.out.println("Expected alert");
+		}
+	}
+}
